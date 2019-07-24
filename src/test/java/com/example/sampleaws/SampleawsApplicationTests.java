f908e0e0fca01634b427cedd525e8a5a9a6851ea
Sivakumar Sethuraj <bharathkumar.b@mookambikainfo.com>
2019-07-24 12:05:10 +0530

    adding test cases

--- src/test/java/com/example/sampleaws/SampleawsApplicationTests.java
+++ src/test/java/com/example/sampleaws/SampleawsApplicationTests.java
@@ -10,7 +10,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 public class SampleawsApplicationTests {
 
 	@Test
-	public void contextLoads() {
+	public void  contextLoads() {
 		double sample = 10.0;
 		assertNotNull(sample);
 	}
